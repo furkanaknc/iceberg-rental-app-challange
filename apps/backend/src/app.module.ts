f22dd672fa4b5e1f065334e50497ad4683516bbe
Furkan Akıncı <furkanaknc55@gmail.com>
2025-08-20 21:13:02 +0300

    feat: add user service function for checking user

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/common/prisma/prisma.module';
 import { EnvironmentModule } from './modules/common/environment/environment.module';
-import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { AuthModule } from './modules/auth/auth.module';
+import { APP_FILTER, APP_GUARD, APP_PIPE } from '@nestjs/core';
+import { JwtGuard } from './common/guards/jwt.guard';
 import { ValidationPipe } from './common/pipes/validation.pipe';
 import { GlobalExceptionFilter } from './common/exception-filters/global-exception.filter';
 import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, EnvironmentModule, UsersModule],
+  imports: [PrismaModule, EnvironmentModule, UsersModule, AuthModule],
   providers: [
     {
       provide: APP_PIPE,
@@ -16,6 +18,10 @@ import { UsersModule } from './modules/users/users.module';
     {
       provide: APP_FILTER,
       useClass: GlobalExceptionFilter,
+    },
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
     },
   ],
 })
