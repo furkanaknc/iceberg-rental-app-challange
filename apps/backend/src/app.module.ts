2dca808e97044b42f8d239d80e12e6317db006ad
Furkan Akıncı <furkanaknc55@gmail.com>
2025-08-20 20:21:54 +0300

    feat: add users service

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from './modules/common/prisma/prisma.module';
 import { EnvironmentModule } from './modules/common/environment/environment.module';
+import { APP_FILTER, APP_PIPE } from '@nestjs/core';
+import { ValidationPipe } from './common/pipes/validation.pipe';
+import { GlobalExceptionFilter } from './common/exception-filters/global-exception.filter';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
-  imports: [PrismaModule, EnvironmentModule],
+  imports: [PrismaModule, EnvironmentModule, UsersModule],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useClass: ValidationPipe,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: GlobalExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
