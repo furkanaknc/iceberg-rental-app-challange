bb20068f26c7d87c20ea9c88fcd98a54a9dd4b71
Furkan Akıncı <123424252+furkanaknc@users.noreply.github.com>
2025-08-25 01:57:20 +0300

    Revert "feat: add jwt guard"
    This reverts commit 2666be6d92e2727bc53eec2c250b4140a46d28bd.

--- apps/backend/src/common/enums/token-type.enum.ts
+++
@@ -1,3 +0,0 @@
-export enum TokenType {
-  Bearer = 'Bearer',
-}
--- apps/backend/src/common/interfaces/token-response.interface.ts
+++
@@ -1,7 +0,0 @@
-import { TokenType } from '../enums/token-type.enum';
-
-export type UserTokenResponse = {
-  access_token: string;
-  expires_in: number;
-  token_type: TokenType;
-};
