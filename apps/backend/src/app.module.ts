f680c16d33410c1042316ec5bb004754de5f05a3
Furkan Akıncı <123424252+furkanaknc@users.noreply.github.com>
2025-08-25 01:57:20 +0300

    Revert "feat: add environment and prisma service to backend"
    This reverts commit bad0715b9812f29736d487cb16412d018aa73522.

--- apps/backend/src/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './modules/common/prisma/prisma.module';
-import { EnvironmentModule } from './modules/common/environment/environment.module';
-
-@Module({
-  imports: [PrismaModule, EnvironmentModule],
-})
-export class AppModule {}
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,18 +1,17 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import { EnvironmentService } from './modules/common/environment/environment.service';
+import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-
-  const envService = app.get(EnvironmentService);
-
-  const port = envService.get('PORT');
-
+  const port = process.env.PORT || 3000;
   await app.listen(port);
   Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
 }
