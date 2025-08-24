48364e8bbbf3535eddb157ecf8a2e2b629c5784f
Furkan Akıncı <123424252+furkanaknc@users.noreply.github.com>
2025-08-25 01:57:20 +0300

    Revert "feat: add pagination and search parameter to backend"
    This reverts commit 66d6881f17709ab584c91cabcd2194d8f16a8ed3.

--- apps/backend/src/common/types/order.type.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaFindManyArgs, PrismaModelName } from './prisma.type';
-
-export type OrderByParams<T extends PrismaModelName> = PrismaFindManyArgs<T>['orderBy'];
--- apps/backend/src/common/types/search-params.type.ts
+++
@@ -1,3 +0,0 @@
-import { PrismaFindManyArgs, PrismaModelName } from './prisma.type';
-
-export type SearchParams<T extends PrismaModelName> = PrismaFindManyArgs<T>['where'];
